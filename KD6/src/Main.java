630d171024f7e3b343c8b4d5f4203c70bed659ce
milenamilosa <milenamilosh@inbox.lv>
2023-10-25 10:42:43 +0300

    October university tasks

---
+++ KD6/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String b = "";
+        double s=2, a=2; int i=2;
+        while(i <= 10) {
+            a = -
+            s = s + a;
+            i = i + 1;
+//            b = a+" " + i + "/" + (i-1) + " + ";
+
+        }
+        System.out.println(s);
+//        System.out.println(b);
+    }
+}
